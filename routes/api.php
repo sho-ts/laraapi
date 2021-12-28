22f5942c40ed9fcfa77ad92965c112b66bc715b7
tagawa shouta <sho.webfe@gmail.com>
2021-12-29 03:58:49 +0900

    ルート更新

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
@@ -16,8 +15,9 @@ use App\Http\Controllers\Api\UserController;
 */
 
 Route::post('/user', [UserController::class, 'register']);
-Route::post('/user/authenticate', [UserController::class, 'authenticate']);
 Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'read']);
 Route::middleware('auth:sanctum')->put('/user', [UserController::class, 'update']);
 Route::middleware('auth:sanctum')->delete('/user', [UserController::class, 'delete']);
-Route::middleware('auth:sanctum')->get('/user/deltoken', [UserController::class, 'deleteToken']);
+
+Route::post('/user/token', [UserController::class, 'authenticate']);
+Route::middleware('auth:sanctum')->delete('/user/token', [UserController::class, 'deleteToken']);
