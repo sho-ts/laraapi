2c671cbbe3969562f459b18c9e7e79e02b2a99b0
tagawa shouta <sho.webfe@gmail.com>
2021-12-29 02:28:43 +0900

    マイグレーションファイル修正

--- database/migrations/2021_12_28_163802_create_users_table.php
+++ database/migrations/2021_12_28_163802_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->timestamps();
             $table->string('name');
             $table->string('email');
-            $table->integer('phone_number');
+            $table->string('phone_number');
             $table->string('password');
         });
     }
