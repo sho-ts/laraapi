6b0c8179b0bb448e65e2a291739bf3a4789df96f
tagawa shouta <sho.webfe@gmail.com>
2021-12-29 02:55:54 +0900

    パスワードの暗号化処理をrepositoryに移動

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -4,14 +4,20 @@ namespace App\Repositories\User;
 
 use App\Repositories\User\UserRepositoryInterface;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
-class UserRepository implements UserRepositoryInterface {
+class UserRepository implements UserRepositoryInterface
+{
     public function __construct(User $user)
     {
         $this->user = $user;
     }
 
-    public function register(array $data) {
+    public function register(array $data)
+    {
+        // パスワードを暗号化
+        $data['password'] = Hash::make($data['password']);
+
         $this->user->create($data);
     }
 }
