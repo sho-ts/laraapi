de348974dadcef480bd1c97b7e8d76b5e47e6df9
tagawa shouta <sho.webfe@gmail.com>
2021-12-29 15:21:35 +0900

    バリデーションエラー時に400を返す様に修正

---
+++ app/Http/Trait/ValidationTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Trait;
+
+use Illuminate\Support\Facades\Validator as ValidationFacade;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+
+trait ValidationTrait
+{
+    protected function userValidation(Request $req, array $rules = [])
+    {
+        $validator = ValidationFacade::make($req->all(), array_merge([
+            'name' => ['required', 'max:32'],
+            'phone_number' => ['required', 'numeric', 'digits_between:8,11'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8'],
+        ], $rules));
+
+        if ($validator->fails()) {
+            $res = response()->json([
+                'status' => 400,
+                'errors' => $validator->errors(),
+            ], 400);
+
+            throw new HttpResponseException($res);
+        }
+    }
+}
